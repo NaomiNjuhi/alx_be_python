6ca64eeac6963147b92a4edcd7fd1aecfc62411a
Naomi Njuhi <nnjuhi@gmail.com>
2025-07-12 15:31:44 +0300

    Fix 1

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+#Perfom arithmetic operations on the 2 variables
+sum = number1 + number2 #Addition
+difference = number1 - number2 #Subtraction
+product = number1 * number2 #Multiplication
+#Print results
+print ("Addition of", number1, "and", number2, "is", sum)
+print ("Subtraction of", number1, "and", number2, "is", difference)
+print ("Multiplication of", number1, "and", number2, "is", product)
