098384c60d0282d9d2424b29302df300514c82cc
Naomi Njuhi <nnjuhi@gmail.com>
2025-07-12 15:56:11 +0300

    Calculate simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+#Calculating simple interest earned
+principal= 1000
+rate= 0.05
+time= 3
+#Simple interest formular (I = P * R * T)
+interest = principal*rate*time
+#Print result
+print ("The simple interest is:", interest)
